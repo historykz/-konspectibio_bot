f66f01c87b4bfce602fbd3a5f60657e081941906
historykz <chatgptalina24@gmail.com>
2026-05-14 00:49:43 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ CallbackQueryHandler, ContextTypes, filters
 from database import Database
 
 logging.basicConfig(
-format=”%(asctime)s - %(name)s - %(levelname)s - %(message)s”,
+format="%(asctime)s – %(name)s – %(levelname)s – %(message)s",
 level=logging.INFO
 )
 logger = logging.getLogger(**name**)
